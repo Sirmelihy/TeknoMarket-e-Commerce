3c890b5f7892af5f760c774bd8bb052d7d89b04b
Asymptote <melihokutangame@hotmail.com>
2023-04-07 17:51:41 +0300

    Berra anasayfa ile birlestirildi + login-register-forgot pass eklendi

---
+++ TeknoMarket/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeknoMarket.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPass()
+        {
+            return View();
+        }
+
+
+    }
+}
