a1008f807a770d4222f792640bea3dc5c01c67d2
Asymptote <melihokutangame@hotmail.com>
2023-06-08 23:31:54 +0300

    test

--- TeknoMarket/Controllers/LoginController.cs
+++ TeknoMarket/Controllers/LoginController.cs
@@ -228,45 +228,5 @@ namespace TeknoMarket.Controllers
             ViewBag.sendHata = sendHata;
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Login(string email, string password)
-        {
-            // Veritabanı bağlantısı
-            string connectionString = ConfigurationManager.ConnectionStrings["teknomarketlogin"].ConnectionString;
-
-            // Veritabanı bağlantısını oluşturma
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                // Veritabanı sorgusu
-                string query = "SELECT * FROM users WHERE Email = @Email AND Password = @Password";
-
-                // Veritabanı komutu 
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    // Parametreler
-                    command.Parameters.AddWithValue("@Email", email);
-                    command.Parameters.AddWithValue("@Password", password);
-
-                    // Veritabanı bağlantısını açma
-                    connection.Open();
-
-                    // Veritabanından verileri okuma
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.HasRows)
-                    {
-                        // Giriş başarılı, kullanıcıyı anasayfaya yönlendirilecek
-                        return RedirectToAction("Anasayfa", "Home");
-                    }
-                    else
-                    {
-                        // Giriş başarısız, hata mesajını
-                        ViewBag.ErrorMessage = "Geçersiz e-posta veya şifre";
-                        return View();
-                    }
-                }
-            }
-        }
     }
 }
